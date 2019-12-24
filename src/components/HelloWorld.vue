ecd2b456b97cbd93966725aaa8f2d7715aba6ef9
zhangmengjuan <zhangmengjuan@example.com>
2019-12-24 15:55:53 +0800

    第一次提交 sass

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="swiper"
+    ref="swiper"
+    @touchstart.prevent.stop = "touchstart"
+    @touchmove.prevent.stop = "touchmove"
+    @touchend.prevent.stop = "touchend"
+    @touchcancel.prevent.stop = "touchcancel"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'swiper',
+  props: {
+    directionFlag: {
+      type: String,
+      default: 'Y'
+    }
+  },
+  components: {
+
+  },
+  data(){
+    return {
+      isDoen: false,
+      start: null,
+      scrollView: null,
+      direction: 'top',
+      offsetWH: 'offsetHeight',
+      offsetTL: 'offsetTop'
+    }
+  },
+  mounted(){
+    this.scrollView = this.$refs.swiper.children[0]
+  },
+  methods: {
+    touchmove(e){
+      if(!this.isDown) return
+      if(!this.directionFlag === 'Y'){
+        this.direction = 'top'
+        this.offsetWH = 'offsetHeight'
+        this.offsetTL = 'offsetLeft'
+      }
+      let end = e.touches[0][`client${this.directionFlag}`]
+      this.distance = end - this.start
+      let distanceNum = this.scrollView[this.offsetTL] + this.distance
+      let distanceMax = this.scrollView[this.offsetWH] - this.scrollView.children[0][this.offsetWH]
+      if(distanceNum < -distanceMax) {
+        this.scrollView.style[this.direction] = -distanceMax + 'px'
+      }else if (distanceNum > 0){
+        this.scrollView.style[this.direction] = 0
+      }else {
+        this.scrollView.style[this.direction] = this.scrollView[this.offsetTL] + this.distance + 'px'
+      }
+      this.start = end
+    },
+    touchstart (e){
+      this.scrollView.classList.remove('animate')
+      this.isDown = true
+      this.start = e.touches[0][`client${this.directionFlag}`]
+      this.start = e.touches[0][`client${this.directionFlag}`]
+    },
+     touchend (e){
+      this.isDown = false
+      this.scrollView.classList.add('animate')
+      this.left = Math.abs[this.scrollView[this.offsetTL]]
+      this.index = Math[this.distance> 0 ? 'floor' : 'ceil'](left/this.scrollView.children[0][this.offsetWH]);
+      this.scrollView.style[this.direction] = -index*this.scrollView.children[0][this.offsetWH] + 'px'
+    }
+  },
+}
+</script>
+<style  lang="scss" scoped>
+.home {
+  width: 100%;
+  height:100%;
+  position:fixed;
+  left:0;
+  overflow: hidden;
+}
+.swiper {
+  width: 100%;
+  height:100%;
+}
+</style>
---
+++ src/components/aside.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="home">
+    <Swiper :directionFlag ="directionFlag">
+    <div class="scroll-view">
+        <div :class="['page' , `page${index+1}`]" v-for="{item,index} in 4" :key="index"></div>
+    </div>
+    </Swiper>
+</div>
+</template>
+<script>
+import fetch from '../utils/fetch'
+import Swiper from '@/components/aside'
+export default {
+  name: 'swiper',
+  components: {
+      Swiper
+  },
+  data(){
+      return {
+          directionFlag: 'x'
+      }
+  },
+  methods: {
+
+  },
+}
+</script>
+<style lang="stylus" scoped>
+.home {
+  width: 100%;
+  height:100%;
+  position:fixed;
+  left:0;
+  overflow: hidden;
+}
+.scroll-view {
+  width: 400%;
+  height:50%;
+  position:absolute;
+  left:0;
+  display :flex;
+  &.animate {
+      transition :all .3s;
+  }
+  .page {
+    width: 100%;
+    height:50%;
+    &1{
+        background :rgb(167 , 5,86)
+    }
+    &2{
+        background :rgb(2 ,127,136)
+    }
+    &3{
+        background :rgb(255 ,153,36)
+    }
+    &4{
+        background :#f66
+    }
+  }
+}
+</style>
